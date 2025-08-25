f956cce1abc69c54774ce1729d0c6a5bc23c18c6
Hanash Ahad <hanashahad22@gmail.com>
2025-08-26 01:14:39 +0500

    Updated project with latest changes

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="Hanash_Ahad_Resume1.pdf"
+                href="Hanash Ahad Resume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
